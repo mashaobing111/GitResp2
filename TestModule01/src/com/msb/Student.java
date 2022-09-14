4dc90064f5020666a50f07edee8df190efd2634d
mashaobing111 <chinamsb@126.com>
2022-09-14 15:54:14 +0800

    第一次提价Student。。

---
+++ TestModule01/src/com/msb/Student.java
@@ -0,0 +1,12 @@
+package com.msb;
+
+/**
+ * @author: msb
+ * @date: 2022/9/14 - 09 - 14 - 15:51
+ * @description: com.msb
+ * @version: 1.0
+ */
+public class Student {
+    private int age;
+    private String name;
+}
